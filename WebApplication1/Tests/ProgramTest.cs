b54f4a1fa8dee56dd1475852c93490dd378934ec
Jeymsson Nogueira Falcao <jeymsson@gmail.com>
2024-12-30 15:02:50 -0300

    feat: Add unit tests for ProgramTest class
    Commands
    add package Microsoft.VisualStudio.TestPlatform.TestFramework
    dotnet add package Microsoft.AspNetCore.Mvc.Testing

---
+++ WebApplication1/Tests/ProgramTest.cs
@@ -0,0 +1,98 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+using WebApplication1.interfaces;
+using WebApplication1.Repository;
+using Xunit;
+using WebApplication1.Service;
+
+namespace WebApplication1.Tests
+{
+    public class ProgramTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public ProgramTest()
+        {
+            _factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                });
+            });
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetSwaggerJson_ReturnsSuccessStatusCode()
+        {
+            // Arrange
+            var request = "/swagger/v1/swagger.json";
+
+            // Act
+            var response = await _client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Contains("\"title\": \"Demo API\"", responseString);
+        }
+
+        [Fact]
+        public async Task GetSecureEndpoint_UnauthorizedWithoutToken()
+        {
+            // Arrange
+            var request = "/api/comment";
+
+            // Act
+            var response = await _client.GetAsync(request);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public void ServicesAreRegistered()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseInMemoryDatabase("TestDatabase")
+            );
+            var inMemorySettings = new Dictionary<string, string?> {
+                {"Jwt:SigningKey", "YourSigningKeyHere"},
+                {"Jwt:Issuer", "YourIssuerHere"},
+                {"Jwt:Audience", "YourAudienceHere"}
+            };
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+
+            services.AddSingleton<IConfiguration>(configuration);
+            services.AddScoped<IStockRepository, StockRepository>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IPortfolioRepository, PortfolioRepository>();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<ApplicationDbContext, ApplicationDbContext>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Act & Assert
+            Assert.NotNull(serviceProvider.GetService<IStockRepository>());
+            Assert.NotNull(serviceProvider.GetService<ICommentRepository>());
+            Assert.NotNull(serviceProvider.GetService<IPortfolioRepository>());
+            Assert.NotNull(serviceProvider.GetService<ITokenService>());
+            Assert.NotNull(serviceProvider.GetService<ApplicationDbContext>());
+        }
+    }
+}
