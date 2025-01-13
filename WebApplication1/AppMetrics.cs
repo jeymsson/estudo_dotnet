11bf45a113778c8c77fe16abede1f4281921ce6e
Jeymsson Nogueira Falcao <jeymsson@gmail.com>
2025-01-13 17:26:01 -0300

    feat: Add OpenTelemetry extensions and configuration
    - Added OpenTelemetry extensions to the project for distributed tracing and metrics collection.
    - Updated appsettings.json to include configuration for enabling OTLP exporter and defining queue hosts.
    - Added AppMetrics class to handle metrics instrumentation.
    - Added GlobalData class to store global application data.
    - Added CustomLogProcessor class to customize log processing.
    - Updated WebApplication1.csproj to include necessary OpenTelemetry packages.
    - Added otel-collector-config.yaml file for configuring the OpenTelemetry collector.
    - Updated docker-compose.yml to include services for OpenTelemetry collector, Jaeger, and Prometheus.

---
+++ WebApplication1/AppMetrics.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace WebApplication1;
+
+public class AppMetrics
+{
+    public AppMetrics(string meterName, string meterVersion)
+    {
+        // Creates a new meter
+        var meter = new Meter(meterName, meterVersion);
+        // Creates a new instrument from the meter
+        VoteCounter = meter.CreateCounter<long>("vote", "count", "Number of votes cast");
+    }
+
+    public Counter<long> VoteCounter { get; }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -8,11 +8,17 @@ using WebApplication1.Data;
 using WebApplication1.Models;
 using WebApplication1.Repository;
 using WebApplication1.Service;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
-using OpenTelemetry.Logs;
 using WebApplication1.Tracing;
 using WebApplication1.interfaces;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Extensions.Docker.Resources;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using WebApplication1;
+using System.Runtime.InteropServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -90,36 +96,118 @@ builder.Services.AddControllers()
         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
     });
 
-builder.Services.AddOpenTelemetry().WithTracing(traceProvider =>
+
+builder.Services.AddSingleton(new AppMetrics(GlobalData.SourceName, GlobalData.ApplicationVersion));
+// Shared resources for OTEL signals
+var resourceBuilder = ResourceBuilder.CreateDefault()
+    // add attributes for the name and version of the service
+    .AddService(GlobalData.ApplicationName, serviceVersion: GlobalData.ApplicationVersion)
+    // add attributes for the OpenTelemetry SDK version
+    .AddTelemetrySdk()
+    // Populate platform details
+    .AddDetector(new DockerResourceDetector())
+    // add custom attributes
+    .AddAttributes(new Dictionary<string, object>
+    {
+        [ResourceSemanticConventions.AttributeHostName] = Environment.MachineName,
+        [ResourceSemanticConventions.AttributeOsDescription] = RuntimeInformation.OSDescription,
+        [ResourceSemanticConventions.AttributeDeploymentEnvironment] =
+            builder.Environment.EnvironmentName.ToLowerInvariant(),
+    });
+
+// Configure logging
+// Set up logging pipeline
+builder.Logging.AddOpenTelemetry(loggerOptions =>
 {
-    traceProvider
-        // .AddSource(OpenTelemetryExtensions.ServiceName)
-        .SetResourceBuilder(
-            ResourceBuilder.CreateDefault()
-                .AddService(
-                    serviceName: OpenTelemetryExtensions.ServiceName,
-                    serviceVersion: OpenTelemetryExtensions.ServiceVersion
-                )
-        )
-        .AddOtlpExporter()
-        .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation()
-        // .AddJaegerExporter(exporter =>
-        // {
-        //     exporter.AgentHost = builder.Configuration["Jaeger:AgentHost"];
-        //     exporter.AgentPort = Convert.ToInt32(builder.Configuration["Jaeger:AgentPort"]);
-        // })
+    loggerOptions.IncludeFormattedMessage = true;
+    loggerOptions.IncludeScopes = true;
+    loggerOptions.ParseStateValues = true;
+    loggerOptions
+        // define the resource
+        .SetResourceBuilder(resourceBuilder)
+        // add custom processor
+        .AddProcessor(new CustomLogProcessor())
+        // send logs to the console using exporter
         .AddConsoleExporter();
+        // send logs to collector if configured
+        if (builder.Configuration.GetValue<bool>("EnableOTLPExporter"))
+        {
+            loggerOptions.AddOtlpExporter(options =>
+                options.Endpoint = new($"http://{builder.Configuration["Hosts:OTLP"]!}:4317"));
+        }
 });
 
-builder.Logging.ClearProviders();
-builder.Logging.AddOpenTelemetry(options =>
-{
-    options.AddOtlpExporter(otlpOptions =>
+// Configure tracing
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder =>
     {
-        otlpOptions.Endpoint = new Uri("http://otel-collector:4317");
-    });
-});
+        tracerProviderBuilder
+            // Sets span status to ERROR on exception
+            .SetErrorStatusOnException()
+            // define the resource
+            .SetResourceBuilder(resourceBuilder)
+            // receive traces from our own custom sources
+            .AddSource(GlobalData.SourceName)
+            // receive traces from built-in sources
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            // ensures that all spans are recorded and sent to exporter
+            .SetSampler(new AlwaysOnSampler())
+            // stream traces to the SpanExporter
+            // BatchActivityExportProcessor processes spans on a separate thread unlike the SimpleActivityExportProcessor
+            .AddProcessor(new BatchActivityExportProcessor(new JaegerExporter(new()
+            {
+                AgentHost = builder.Configuration["Hosts:Jaeger"],
+
+                /*
+                // Select between Jaeger or OTLP SpanExporter
+                builder.Configuration.GetValue<bool>("EnableOTLPExporter")
+                    // Sends metrics to an OTLP endpoint.
+                    // Use this to send traces to the OTEL collector.
+                    ? new OtlpTraceExporter(
+                        new() { Endpoint = new($"http://{builder.Configuration["Hosts:OTLP"]!}:4317") })
+                    : new JaegerExporter(new() { AgentHost = builder.Configuration["Hosts:Jaeger"] })));
+                */
+            })));
+    })
+    // Configure metrics
+    .WithMetrics(meterProviderBuilder =>
+    {
+        meterProviderBuilder
+            // add rich tags to our metrics
+            .SetResourceBuilder(resourceBuilder)
+            // receive metrics from built-in sources
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            // receive metrics from custom sources
+            .AddMeter(GlobalData.SourceName)
+            // add view to customize output
+            // uncomment the following code to enable the view
+            //.AddView(instrument =>
+            //{
+            //    // remove all instruments except vote
+            //    if (instrument.Name != "vote")
+            //    {
+            //        return MetricStreamConfiguration.Drop;
+            //    }
+
+            //    if (instrument.Name == "vote")
+            //    {
+            //        // customize the vote instrument
+            //        return new
+            //        {
+            //            Name = "app_vote",
+            //            // remove all dimensions except host
+            //            TagKeys = new[] { "host" },
+            //        };
+            //    }
+
+            //    return null;
+            //})
+            // expose metrics in Prometheus exposition format
+            .AddPrometheusExporter();
+    })
+    .StartWithHost();
 
 var app = builder.Build();
 
@@ -129,6 +217,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Enable the /metrics endpoint which will be scraped by Prometheus
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
+// Endpoint to record exception
+app.MapGet("/exception/", (TracerProvider tracerProvider, ILogger<Program> logger) =>
+    {
+        var tracer = tracerProvider.GetTracer(GlobalData.SourceName);
+        using var span = tracer.StartActiveSpan("Exception span");
+        var simulatedException = new ApplicationException("Error processing the request");
+        span.RecordException(simulatedException);
+        span.SetStatus(Status.Error);
+        logger.LogError(simulatedException, "Error logged");
+        return Results.Ok();
+    })
+    .WithName("Exception")
+    .Produces(StatusCodes.Status200OK);
 
 app.UseAuthentication();
 app.UseAuthorization();
