6a99b95b8e9c881c9162f51bfe40211f2c9ae22e
Jeymsson Nogueira Falcao <jeymsson@gmail.com>
2025-01-17 16:54:39 -0300

    feat: add opentelemetry support for tracing with jaeger
    - Added otel.yml configuration file for OpenTelemetry
    - Added GlobalData.cs for storing global application data
    - Updated Program.cs to configure OpenTelemetry tracing
    - Updated WebApplication1.csproj to include OpenTelemetry dependencies
    - Updated appsettings.json to include Jaeger and OTLP configuration
    - Updated docker-compose.yml to include Jaeger and OTLP services

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,15 +1,27 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using WebApplication1;
 using WebApplication1.Data;
 using WebApplication1.interfaces;
 using WebApplication1.Models;
 using WebApplication1.Repository;
 using WebApplication1.Service;
 using Scalar.AspNetCore;
+using WebApplication1.Tracing;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Extensions.Docker.Resources;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Runtime.InteropServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -87,6 +99,52 @@ builder.Services.AddControllers()
         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
     });
 
+builder.Services.AddSingleton(new AppMetrics(GlobalData.SourceName, GlobalData.ApplicationVersion));
+// Shared resources for OTEL signals
+var resourceBuilder = ResourceBuilder.CreateDefault()
+    // add attributes for the name and version of the service
+    .AddService(GlobalData.ApplicationName, serviceVersion: GlobalData.ApplicationVersion)
+    // add attributes for the OpenTelemetry SDK version
+    .AddTelemetrySdk()
+    // Populate platform details
+    .AddDetector(new DockerResourceDetector())
+    // add custom attributes
+    .AddAttributes(new Dictionary<string, object>
+    {
+        [ResourceSemanticConventions.AttributeHostName] = Environment.MachineName,
+        [ResourceSemanticConventions.AttributeOsDescription] = RuntimeInformation.OSDescription,
+        [ResourceSemanticConventions.AttributeDeploymentEnvironment] =
+            builder.Environment.EnvironmentName.ToLowerInvariant(),
+    });
+
+
+// Check if the console exporter is enabled
+bool isConsoleExporterEnabled = bool.Parse(
+    builder.Configuration["OTLP:ConsoleExporter"] ?? "false"
+);
+// Configure tracing and metrics
+builder.Services
+    .AddOpenTelemetry()
+    .WithMetrics(meterProviderBuilder => {
+        meterProviderBuilder
+            // add rich tags to our metrics
+            .SetResourceBuilder(resourceBuilder)
+            // receive metrics from built-in sources
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            // receive metrics from custom sources
+            .AddMeter(GlobalData.SourceName)
+            // expose metrics in Prometheus exposition format
+            .AddPrometheusExporter();
+
+        // stream metrics to the console
+        if(isConsoleExporterEnabled) {
+            meterProviderBuilder.AddConsoleExporter();
+        }
+    });
+
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -100,9 +158,22 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
+// Enable the /metrics endpoint which will be scraped by Prometheus
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
+
+app.MapHealthChecks("/health", new HealthCheckOptions {
+    AllowCachingResponses = false,
+    ResultStatusCodes = {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Degraded] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+    }
+});
+
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseHttpsRedirection();
 app.MapControllers();
+
 
 app.Run();
